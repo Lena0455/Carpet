9fbf622273d2a75b16a43c9c9e94f30e1e6e46f6
Lena Zavyalova <zavyalovalena28@gmail.com>
2022-04-17 08:55:21 +0300

    add product-color

--- Source/js/script.js
+++ Source/js/script.js
@@ -3,13 +3,13 @@ const e = document.querySelectorAll(".delivery-column > div")
 
 console.log(e)
 
-// function toggle(el, classname) {
-//     if (el.classList.contains(classname)) {
-//         el.classList.remove(classname)
-//     } else {
-//         el.classList.add(classname)
-//     }
-// }
+/*function toggle(el, classname) {
+    if (el.classList.contains(classname)) {
+        el.classList.remove(classname)
+    } else {
+        el.classList.add(classname)
+    }
+}*/
 
 e.forEach((el) => {
     el.addEventListener("click", (event) => {
@@ -19,6 +19,43 @@ e.forEach((el) => {
     })
 })
 
-
-
 // let a = [1, 2, 3, "vasiliy", true, 2003]
+/*const htmlElement = document.documentElement;
+const headElement = document.head;
+const bodyElement = document.body;
+console.log(htmlElement);
+console.log(headElement);
+console.log(bodyElement);
+const childNodes = bodyElement.childNodes;
+console.log(bodyElement.hasChildNodes());*/
+
+// // получить элемент
+// let red = document.getElementById('red');
+//
+// // сделать его фон красным
+// red.style.background = 'red';
+
+
+/*let elements = document.querySelectorAll('.title');
+for (let elem of elements) {
+    alert(elem.innerHTML); // "тест", "пройден"
+}*/
+
+// element.addEventListener(event, handler[, options]);
+
+// [button1, button2, button3] [button1] []
+
+let buttons = document.querySelectorAll('.success');
+for (const button of buttons) {
+    button.onclick = () => alert("Привет");
+    button.addEventListener("click", handler1); // Спасибо!
+    button.addEventListener("click", handler2); // Спасибо ещё раз!
+}
+
+function handler1() {
+    alert('Спасибо!');
+};
+
+function handler2() {
+    alert('Спасибо ещё раз!');
+}
